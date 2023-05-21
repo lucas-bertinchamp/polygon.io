ff706e8158b607706c6f49238b3ae64074daf62e
TeddyBeer94 <achenisbest@free.fr>
2023-05-21 17:54:05 +0200

    personal save

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import PixiComponent from "@/components/PixiComponent";
 import Head from "next/head";
-import { useEffect } from "react";
+//import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
