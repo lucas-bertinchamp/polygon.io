b007b78d052cb9b4677ddd3570b459b024a04c78
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2023-05-23 09:36:05 +0200

    test deploy

--- components/PixiComponent.js
+++ components/PixiComponent.js
@@ -7,6 +7,9 @@ import Projectile from "./Projectile";
 import Bars from "./Bars";
 import BarsUtils from "./utils/BarsUtils";
 import Leaderboard from "./Leaderboard";
+import { io } from "socket.io-client";
+
+const socketClient = io.connect(" https://polygonio.herokuapp.com/");
 
 const PixiComponent = ({ gameData }) => {
   // interact with the bars
